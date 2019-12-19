ea61b3c7943334d0e14b8265b2c8aa9cf1023119
Lawrence-Tang <tangzongsheng@gmail.com>
2019-12-19 14:20:20 +0800

    add uboot testing sciprt

---
+++ test/test-update-uboot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eux
+
+# HTTP_SERVER=112.124.9.243
+HTTP_SERVER=192.168.1.9
+
+# clean
+mkdir -p tmp
+sudo rm -rf tmp/*
+
+cd tmp
+git clone ../../.git sd-fuse_s5p6818
+cd sd-fuse_s5p6818
+wget http://${HTTP_SERVER}/dvdfiles/S5P6818/images-for-eflasher/friendlycore-arm64-images.tgz
+tar xzf friendlycore-arm64-images.tgz
+
+git clone https://github.com/friendlyarm/u-boot --depth 1 -b nanopi2-v2016.01 uboot-s5p6818
+
+UBOOT_SRC=$PWD/uboot-s5p6818 ./build-uboot.sh friendlycore-arm64
+sudo ./mk-sd-image.sh friendlycore-arm64
