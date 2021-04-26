1466a7003133c148bf1ae889c32ec5c4b66031e1
Lawrence-Tang <tangzongsheng@gmail.com>
2021-04-26 13:58:09 +0800

    add test script for keep logo on for a longer time

---
+++ test/test-update-kernel-for-logo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -eux
+
+# HTTP_SERVER=112.124.9.243
+HTTP_SERVER=192.168.1.9
+
+# clean
+mkdir -p tmp
+sudo rm -rf tmp/*
+
+cd tmp
+git clone ../../.git sd-fuse_s5p6818
+cd sd-fuse_s5p6818
+wget http://${HTTP_SERVER}/dvdfiles/S5P6818/images-for-eflasher/friendlycore-arm64-images.tgz
+tar xzf friendlycore-arm64-images.tgz
+
+# git clone https://github.com/friendlyarm/linux -b nanopi2-v4.4.y --depth 1 kernel-s5p6818
+git clone git@192.168.1.5:/devel/kernel/linux.git --depth 1 -b nanopi2-v4.4.y kernel-s5p6818
+# disable framebuffer console support, keep logo on for a longer time
+sed -i -e 's/CONFIG_FRAMEBUFFER_CONSOLE=y/CONFIG_FRAMEBUFFER_CONSOLE=n/g' kernel-s5p6818/arch/arm64/configs/nanopi3_linux_defconfig
+KERNEL_SRC=$PWD/kernel-s5p6818 ./build-kernel.sh friendlycore-arm64
+sudo ./mk-sd-image.sh friendlycore-arm64
