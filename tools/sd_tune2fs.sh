96de98fd23f86c6dd3d5a8683992438344777212
Lawrence-Tang <tangzongsheng@gmail.com>
2021-05-10 13:39:13 +0800

    fixed issue that occur when mkfs calculate rootfs size (batch updates)

--- tools/sd_tune2fs.sh
+++ tools/sd_tune2fs.sh
@@ -20,7 +20,7 @@
 # ----------------------------------------------------------
 # Checking block device
 
-if [ $# -eq 0 ]; then
+if [ -z $1 ]; then
 	echo "Usage: sd_tune2fs.sh <SD Reader's device>"
 	exit 0
 fi
